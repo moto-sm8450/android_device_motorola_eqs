9b9629fe6a4c2525c61e892a9ba6611eb4ba6da1
Bruno Martins <bgcngm@gmail.com>
2025-01-27 09:45:36 +0000

    eqs: Rename rootdir to init and migrate to blueprints
    Change-Id: I9d55dc634fc4df2c60d30a0c18ec356a857e3c4b

--- device.mk
+++ device.mk
@@ -55,12 +55,9 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.moto_sm8475_fod
 
 # Init
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/hw/*.rc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/*.rc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
+PRODUCT_PACKAGES += \
+    init.mmi.overlay.rc \
+    init.vendor.st21nfc.rc
 
 # NFC
 PRODUCT_PACKAGES += \
